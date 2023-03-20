7d099c66ab9014ef09b2a1472b949f790a6eccb4
Dillon Depeel <dillondepeel@hotmail.com>
2023-03-20 12:43:40 -0700

    Add files via upload

---
+++ hello.adb
@@ -0,0 +1,6 @@
+-- Ada Hello, World! program.
+with Text_IO; use Text_IO;      -- This gets the IO facility.
+procedure Hello is              -- Main, but no special name is needed.
+begin
+   Put_Line("Hello, World!");   -- Put_Line is from Text_IO.
+end Hello;
